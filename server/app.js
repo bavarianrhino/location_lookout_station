4df470f77064e7cd4dd33d6320614bab46ea7d97
bavarianrhino <rjriesenberger@gmail.com>
2020-02-01 10:51:00 -0500

    setup temp server backend

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const userName = 'Ryan';
+fs.writeFile('user-data.txt', 'Name: ' + userName, (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('WROTE FILE');
+});
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log('INCOMING REQUEST');
+    console.log(req.method, req.url);
+    
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Success!</h1>');
+});
+
+server.listen(5000);
