17af8a6f96664ab53b4d43b916a01095114073c2
bavarianrhino <rjriesenberger@gmail.com>
2020-02-01 05:30:49 -0500

    added new places route and switch and redirect

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
 import Users from './user/pages/Users'
+import NewPlace from './places/pages/NewPlace'
 
 const App = () => {
     return (
         <Router>
-            <Route path='/'exact={true}><Users /></Route>
-            <Redirect to="/" />
+            <Switch>
+                <Route path='/' exact={true}><Users /></Route>
+                <Route path='/places/new' exact={true}><NewPlace /></Route>
+                <Redirect to="/" />
+            </Switch>
         </Router>
     )
 }
