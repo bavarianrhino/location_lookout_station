7fdbc9888cbeca13209025010cd3f1bcff46bf2d
bavarianrhino <rjriesenberger@gmail.com>
2020-02-01 11:12:28 -0500

    added link import

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-do
 
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace'
+import MainNavigation from './shared/components/Navigation/MainNavigation'
 
 const App = () => {
     return (
         <Router>
-            <Switch>
-                <Route path='/' exact={true}><Users /></Route>
-                <Route path='/places/new' exact={true}><NewPlace /></Route>
-                <Redirect to="/" />
-            </Switch>
+            <MainNavigation />
+            <main>
+                <Switch>
+                    <Route path='/' exact={true}><Users /></Route>
+                    <Route path='/places/new' exact={true}><NewPlace /></Route>
+                    <Redirect to="/" />
+                </Switch>
+            </main>
         </Router>
     )
 }
--- client/src/shared/components/Navigation/MainNavigation.js
+++ client/src/shared/components/Navigation/MainNavigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 import MainHeader from './MainHeader'
 import './MainNavigation.css';
