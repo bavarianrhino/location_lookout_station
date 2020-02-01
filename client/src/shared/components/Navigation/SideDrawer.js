727a6bff6c300342bcc6130247bc600da9f882eb
bavarianrhino <rjriesenberger@gmail.com>
2020-02-01 11:25:37 -0500

    added sidedrawer logic

---
+++ client/src/shared/components/Navigation/SideDrawer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { NavLink } from 'react-router-dom'
+
+// import MainHeader from './MainHeader'
+import './SideDrawer.css';
+
+const SideDrawer = props => {
+    return (
+        <aside className="side-drawer"></aside>
+    )
+}
+export default SideDrawer;
