8557ae7e26aed107a12b9b88950341f1e824382f
bavarianrhino <rjriesenberger@gmail.com>
2020-02-01 06:11:46 -0500

    added useritem and userslist jsx to render info

--- client/src/user/components/UsersList.js
+++ client/src/user/components/UsersList.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import UserItem from './UserItem'
 import './UsersList.css'
 
 const UsersList = props => {
+    if (props.items.length === 0) {
+        return (
+            <div className="center">
+                <h2>No Users Found</h2>
+            </div>
+        )
+    }
 
+    return (
+        <ul className="user-list">
+            {props.items.map(user => (
+                <UserItem key={user.id} id={user.id} image={user.image} name={user.name} placeCount={user.places} />
+            ))}
+        </ul>
+    )
 }
 export default UsersList
--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import UsersList from '../components/UsersList'
+
 const Users = () => {
+
+    // Dummy Data
+    // const USERS = [];
+    const USERS = [{id: 'u1', name: "Ryan Riesenberger", image: "https://www.bobross.com/v/vspfiles/templates/bobrossjoy/images/template/home_bob.png", places: 3}];
+
     return (
-        <h1>Users works</h1>
+        <UsersList items={USERS} />
     )
 }
 export default Users;
