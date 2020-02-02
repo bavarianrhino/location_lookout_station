3e1ab923c501f6539af8888480e32c48fd0f4172
bavarianrhino <rjriesenberger@gmail.com>
2020-02-02 02:30:53 -0500

    added error handler for unknown route

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error')
 const usersRoutes = require('./routes/users-routes');
 
 const app = express();
@@ -9,6 +10,12 @@ const app = express();
 app.use(bodyParser.json())
 
 app.use('/api/places', placesRoutes); // => /api/places...
+
+// Handles unknown route
+app.use((req, res, next) => {
+    const error = new HttpError("Could not find this route!", 404)
+    throw error;
+})
 
 // Express knows when you pass a use function 4 parameters, it knows to use it as an error middleware.
 app.use((error, req, res, next) => {
